d432c3e6d20848aef6f3bd708c92c27a33aea684
Ilya201091 <ZiK201091@ya.ru>
2022-08-23 00:30:52 +0700

    домашняя работа

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Домашнее задание 3!");
+        // Задача 1
+        int clientOS = 1;
+        if (clientOS == 0) {System.out.println("Установите версию приложения для iOS по ссылке");}
+        else if (clientOS == 1) {System.out.println("Установите версию приложения для Android по ссылке");}
+
+        // Задача 2
+        int clientDeviceYear = 2015;
+        if (clientDeviceYear < 2015) {System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+        else { if (clientDeviceYear <= 2015) System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+
+        //Задача 3
+
+
+
+
+
+
+
     }
 }
